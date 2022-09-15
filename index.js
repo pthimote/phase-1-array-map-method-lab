16794c0b1c9705a0acc5a513a3a711751f8cf9de
Paul Thimote <pthimote@gmail.com>
2022-09-15 12:05:47 -0400

    commit tree dbceff3b61479598d8fa1bffe2652f9cd74644c2

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+  const titleCased = () => {
+  return tutorials.map(title => title.split(' ').map(function (word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }).join(' '))
 }
+
+
+// const array1 = [1, 4, 9, 16];
+// // pass a function to map
+// const map1 = array1.map(x => x * 2);
+// console.log(map1);
+// // expected output: Array [2, 8, 18, 32]
+
+
+
+// function toTitleCase(str) {
+//   return str.toLowerCase().split(' ').map(function (word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' ');
+// }
+// console.log(toTitleCase("welcome to w3docs"));
